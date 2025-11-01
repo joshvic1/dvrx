6e5ea3c134c7d2ed1def199f08adeae46e23de62
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-11-01 19:34:38 +0100

    updated page

--- pages/whatsapp.js
+++ pages/whatsapp.js
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function RedirectToWhatsApp() {
-  const whatsappDeepLink = "whatsapp://chat?code=GJQ1wMwkvV9G4vnYF84qF3";
-  const whatsappFallback = "https://chat.whatsapp.com/GJQ1wMwkvV9G4vnYF84qF3";
+  const whatsappDeepLink = "whatsapp://chat?code=HPC1lFn3tZ30pp6tRmFJyI";
+  const whatsappFallback = "https://chat.whatsapp.com/HPC1lFn3tZ30pp6tRmFJyI";
   const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/tiktok-event`;
 
   useEffect(() => {
