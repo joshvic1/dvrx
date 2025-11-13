8b1cce2bef485d0c3bfac57bbca28d53f89f1dea
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2025-11-13 17:29:06 +0100

    updated page

--- pages/joshspotmedia.js
+++ pages/joshspotmedia.js
@@ -8,8 +8,8 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function JoinWhatsAppClassPage() {
   const [loading, setLoading] = useState(false);
 
-  const whatsappDeepLink = "whatsapp://chat?code=J3uyscrAbx69BtyLcprHoS";
-  const whatsappFallback = "https://chat.whatsapp.com/J3uyscrAbx69BtyLcprHoS";
+  const whatsappDeepLink = "whatsapp://chat?code=Jqyr7iuNfSKCAFUWGgkX2z";
+  const whatsappFallback = "https://chat.whatsapp.com/Jqyr7iuNfSKCAFUWGgkX2z";
   const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/tiktok-event`;
 
   const handleClick = async () => {
