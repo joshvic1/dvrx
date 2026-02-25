375ed466cddfa875e2c5c1a1b452c5b5ae8b995f
Olatunbosun Joshua <38948371+joshvic1@users.noreply.github.com>
2026-02-25 12:46:10 +0100

    Update WhatsApp links to new URL

--- pages/whats.js
+++ pages/whats.js
@@ -6,8 +6,8 @@ export default function JoinWhatsAppPage() {
   const [loading, setLoading] = useState(false);
 
   // 👉 Replace with your WhatsApp group ieeeee OR directddttt chat link
-  const whatsappLink = "https://chat.whatsapp.com/JJmphSlklLf3yt0AuxLbhn";
-  const whatsappDeepLink = "whatsapp://chat?code=JJmphSlklLf3yt0AuxLbhn";
+  const whatsappLink = "https://mytiklink.com/r/ehr3bg";
+  const whatsappDeepLink = "https://mytiklink.com/r/ehr3bg";
   const handleClick = () => {
     setLoading(true);
 
